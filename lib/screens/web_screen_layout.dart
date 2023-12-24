632c8bb2ea7027c474614b0baf303291ddfdffef
Sanjay Singha <sanjay.singha@karkinos.in>
2023-12-24 15:49:04 +0530

    adding mobile screen

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:wahstapp_ui/colors.dart';
+import 'package:wahstapp_ui/widgets/chat_list.dart';
+import 'package:wahstapp_ui/widgets/contacts_list.dart';
+import 'package:wahstapp_ui/widgets/web_chat_app_bar.dart';
+import 'package:wahstapp_ui/widgets/web_profile_bar.dart';
+import 'package:wahstapp_ui/widgets/web_search_bar.dart';
 
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({super.key});
@@ -6,7 +12,103 @@ class WebScreenLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("This is web app")),
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  //Web profile bar
+                  WebProfileBar(),
+                  //Web search bar
+                  WebSearchBar(),
+                  ContactList()
+                ],
+              ),
+            ),
+          ),
+          //Web Screen
+          Container(
+            ///settign with to take 75 % width of the screen
+            width: MediaQuery.of(context).size.width * 0.75,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage(
+                    'assets/backgroundImage.png',
+                  ),
+                  fit: BoxFit.cover),
+            ),
+            child: Column(
+              children: [
+                //chat app bar
+                const WebChatAppBar(),
+
+                //chat List
+                const Expanded(child: ChatList()),
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.07,
+                  padding: const EdgeInsets.all(10),
+                  decoration: const BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(color: dividerColor),
+                    ),
+                    color: chatBarMessage,
+                  ),
+                  child: Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.emoji_emotions_outlined,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.attach_file,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                            left: 10,
+                            right: 15,
+                          ),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: searchBarColor,
+                              hintText: 'Type a message',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20.0),
+                                borderSide: const BorderSide(
+                                  width: 0,
+                                  style: BorderStyle.none,
+                                ),
+                              ),
+                              contentPadding: const EdgeInsets.only(left: 20),
+                            ),
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.mic,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/widgets/contacts_list.dart
+++ lib/widgets/contacts_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:wahstapp_ui/colors.dart';
 import 'package:wahstapp_ui/info.dart';
+import 'package:wahstapp_ui/screens/mobile_chat_screen.dart';
 
 class ContactList extends StatelessWidget {
   const ContactList({super.key});
@@ -8,44 +9,50 @@ class ContactList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 10),
-      child: Padding(
-        padding: const EdgeInsets.only(bottom: 8.0),
-        child: Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: info.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(info[index]['name'].toString()),
-                    subtitle: Padding(
-                      padding: const EdgeInsets.only(top: 6),
-                      child: Text(
-                        info[index]['message'].toString(),
-                        style: const TextStyle(
-                          fontSize: 15,
+      padding: const EdgeInsets.fromLTRB(0, 10, 0, 0.8),
+      child: Column(
+        children: [
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: info.length,
+            itemBuilder: (context, index) {
+              return Column(
+                children: [
+                  InkWell(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => MobileChatScreen()),
+                      );
+                    },
+                    child: ListTile(
+                      title: Text(info[index]['name'].toString()),
+                      subtitle: Padding(
+                        padding: const EdgeInsets.only(top: 6),
+                        child: Text(
+                          info[index]['message'].toString(),
+                          style: const TextStyle(
+                            fontSize: 15,
+                          ),
                         ),
                       ),
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(info[index]['profilePic'].toString()),
+                      ),
+                      trailing: Text(
+                        info[index]['time'].toString(),
+                        style: const TextStyle(fontSize: 13, color: Colors.grey),
+                      ),
                     ),
-                    leading: CircleAvatar(
-                      backgroundImage: NetworkImage(info[index]['profilePic'].toString()),
-                    ),
-                    trailing: Text(
-                      info[index]['time'].toString(),
-                      style: const TextStyle(fontSize: 13, color: Colors.grey),
-                    ),
-                  );
-                },
-              ),
-            ),
-            const Divider(
-              color: dividerColor,
-              indent: 85,
-            )
-          ],
-        ),
+                  ),
+                  const Divider(
+                    color: dividerColor,
+                    indent: 85,
+                  )
+                ],
+              );
+            },
+          ),
+        ],
       ),
     );
   }
